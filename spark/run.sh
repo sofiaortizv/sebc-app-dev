8eb5900cfc1efcfcaaa1e9d1753e406c61a610e8
chofora <chofora@chofis-lap>
2018-05-15 18:16:20 -0500

    Ejercicio spark

--- spark/run.sh
+++ spark/run.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 SPARK_CLASS=com.cloudera.sdk.SparkBatchExample
-SPARK_BUILD_DIR=target/scala-2.10
-SPARK_JAR=sparkbatchexample_2.10-1.0.jar
-SPARK_BASE=input/
+SPARK_BUILD_DIR=${PWD}
+SPARK_JAR=sparkbatchexample_2.11-1.0.jar
+DIR_INPUT=spark/input/file*.csv
+DIR_OUTPUT=spark/output/
 
-DEPLOY_MODE=xxx
-MASTER=xxx
+DEPLOY_MODE=cluster
+MASTER=yarn
 
-spark-submit --deploy-mode ${DEPLOY_MODE} --master ${MASTER} --executor-memory 128M --num-executors 2 --executor-cores 1 --class ${SPARK_CLASS} ${SPARK_BUILD_DIR}/${SPARK_JAR} ${SPARK_BASE}
+spark-submit --deploy-mode ${DEPLOY_MODE} --master ${MASTER} --executor-memory 128M --num-executors 2 --executor-cores 1 --class ${SPARK_CLASS} ${SPARK_BUILD_DIR}/${SPARK_JAR} ${DIR_INPUT} ${DIR_OUTPUT}
--- spark/src/main/scala/com/cloudera/sdk/SparkBatchExample.scala
+++ spark/src/main/scala/com/cloudera/sdk/SparkBatchExample.scala
@@ -1,27 +1,41 @@
 package com.cloudera.sdk
 
-import org.apache.spark.{SparkConf, SparkContext }
+import org.apache.spark.{ SparkConf, SparkContext }
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.fs.{ FileSystem, Path }
 import org.apache.spark.rdd.RDD
 
 object SparkBatchExample {
-    def main(args: Array[String]): Unit = {
-        if (args.length < 1) {
-            System.err.println("Usage: SparkBatchExample <path>")
-            System.exit(1)
-        }
-
-        val path = args(0)
-        val conf = new SparkConf()
-            .setAppName("Spark Batch Example")
-            .setMaster("local[2]")
-            .set("spark.io.compression.codec", "lz4")
-        val sc = new SparkContext(conf)
- 
-        conf.set("spark.cleaner.ttl", "120000")
-
-        /* READ DATA FROM HDFS, SORT BY KEY, SAVE TO HDFS */
+  def main(args: Array[String]): Unit = {
+    if (args.length < 1) {
+      System.err.println("Usage: SparkBatchExample <path>")
+      System.exit(1)
     }
+
+    val pathInput = args(0)
+    val pathOutput = args(1)
+    val conf = new SparkConf()
+      .setAppName("Spark Batch Example")
+      .set("spark.io.compression.codec", "lz4")
+    val sc = new SparkContext(conf)
+    conf.set("spark.cleaner.ttl", "120000")
+
+    
+    /* CLEAN DESTINATION FOLDER */
+    val fs=FileSystem.get(sc.hadoopConfiguration)
+    fs.delete(new Path(pathOutput),true)
+    
+    /* READ DATA FROM HDFS, SORT BY KEY, SAVE TO HDFS */
+    val words = sc.textFile(pathInput).repartition(1).map {
+      line =>
+        val token = line.split(",")
+        (token(0), token(1))
+    }.sortByKey()
+
+    words.saveAsTextFile(pathOutput)
+    
+    sc.stop
+
+  }
 }
 
